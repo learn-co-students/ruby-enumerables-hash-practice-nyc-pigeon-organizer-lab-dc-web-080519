c4bdaf5f1043697572917f60c0c7f164366e9515
Pete Hanner <petehanner@gmail.com>
2019-06-22 11:10:53 -0700

    Wrote function to reorganize pigeon data

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # create empty hash for eventual return
+  pigeon_list = {}
+
+  # nested each statements loop over every name
+  data.each_value do |subhash|
+    subhash.each_value do |name_array|
+      name_array.each do |name|
+        # add name to return hash if not already there
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        end
+      end
+    end
+  end
+
+  # list of pigeon names to test
+  pigeon_list_names = pigeon_list.keys
+  # nested each statments check for each name in attributes and add value to key when name is found
+  pigeon_list_names.each do |name|
+    data.each do |key, value|
+      value.each do |attribute, name_array|
+        if name_array.include?(name)
+          pigeon_list[name][key].push(attribute.to_s)
+        end
+      end
+    end
+  end
+  # return updated hash
+  pigeon_list
 end
