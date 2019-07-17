1fef06fd911a799e1e1bc87e884b4a8e63f84fc7
Flatiron School <flatironschool@Flatirons-Air.fios-router.home>
2019-07-17 14:55:28 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newHash = {}
+  data.each do |levelOne, value|
+    value.each do |levelTwo, value|
+      value.each do |name|
+        if newHash[name] == nil
+          newHash[name] = {}
+          newHash[name][levelOne] = []
+        else
+          newHash[name][levelOne] = []
+        end
+      end
+    end
+  end
+  newHash.each do|name, values|
+    values.each do |levelTwo, array|
+      data.each do|atributes, values|
+        values.each do |value, array|
+          array.each do |element|
+          if element == name && levelTwo == atributes
+            newHash[name][levelTwo] << value.to_s
+          end
+        end
+        end
+      end
+    end
+  end
+  newHash
 end
+
+
+
+    
+
+
