410067c8f8416a809e78fff7836be6a5506ba644
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-31 17:09:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  byname = {}
+  data.each do |key, category|
+    category.each do |attribute, names|
+      names.each do |name|
+        if !byname[name]
+        byname[name] = {}
+        end
+        if !byname[name][key]
+        byname [name][key] = []
+        end
+        byname[name][key].push("#{attribute}")
+      end
+    end
+  end
+  byname
 end
+
